7baf3509d737f7ef785e861edd361bc69a0b5365
wackyx <515937493@qq.com>
2023-02-11 01:15:07 +0800

    更新路由

--- main.go
+++ main.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"strings"
+	"gohub/bootstrap"
 )
 
 //main
 func main() {
-	// 初始化 Gin 实例
-	r := gin.New()
-	r.Use(gin.Logger(), gin.Recovery())
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
 
-		// 以 JSON 格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
+	// new 一个 Gin Engine 实例
+	router := gin.New()
 
-	// 处理 404 请求
-	r.NoRoute(func(c *gin.Context) {
-		// 获取标头信息的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
-
-
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	r.Run(":8000")
+	err := router.Run(":8080")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
+
+
