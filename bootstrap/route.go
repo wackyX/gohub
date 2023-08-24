55aea9ce62cc4f6e9b66b0d0c3b9b9e9d2df6ce1
wackyx <515937493@qq.com>
2023-08-24 13:36:04 +0800

    HTTP 访问日志

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"gohub/app/http/middlewares"
 	"gohub/routes"
 	"net/http"
 	"strings"
@@ -23,7 +24,7 @@ func SetupRoute(router *gin.Engine) {
 
 func registerGlobalMiddleWare(router *gin.Engine) {
 	router.Use(
-		gin.Logger(),
+		middlewares.Logger(),
 		gin.Recovery(),
 	)
 }
@@ -45,4 +46,3 @@ func setup404Handler(router *gin.Engine) {
 		}
 	})
 }
-
--- main.go
+++ main.go
@@ -17,6 +17,11 @@ func init() {
 
 func main() {
 
+	// 设置 gin 的运行模式，支持 debug, release, test
+	// release 会屏蔽调试信息，官方建议生产环境中使用
+	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
+	// 故此设置为 release，有特殊情况手动改为 debug 即可
+	gin.SetMode(gin.ReleaseMode)
 	// 配置初始化，依赖命令行 --env 参数
 	var env string
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
@@ -41,4 +46,5 @@ func main() {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
 	}
+
 }
